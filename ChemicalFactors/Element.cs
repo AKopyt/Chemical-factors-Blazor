104a33b18f2b0578ce997fc9a246413c853f29a6
AKopyt <agatakopyt@op.pl>
2022-11-01 19:43:19 +0100

    refactor

---
+++ ChemicalFactors/Element.cs
@@ -0,0 +1,13 @@
+﻿namespace ChemicalFactors
+{
+    public class Element
+    {
+        public Element(string symbol)
+        {
+            Symbol = symbol;
+        }
+        public string Symbol { get; set; }
+
+
+    }
+}
